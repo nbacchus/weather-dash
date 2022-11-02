1a42edf367e8837a061916be9c37e06e6bf6347e
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-02 13:33:32 -0400

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,160 @@
+// array to hold the users search history
 let searchHistory = [];
 let lastCitySearched = "";
 
-//API call
+//API Call
 let getCityWeather = function (city) {
-  let apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=c107fd0ee983a34881f3573465d57ca5&units=imperial";
+  // format the OpenWeather api url
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=c107fd0ee983a34881f3573465d57ca5&units=imperial";
+
   fetch(apiUrl)
-    .then(function (response) {
-      // request was successful
+    .then(function(response) {
+      //successful
       if (response.ok) {
-        response.json().then(function (data) {
+        response.json().then(function(data) {
           displayWeather(data);
         });
-        // request fails
+        //request fails
       } else {
         alert("Error: " + response.statusText);
       }
+    })
+
+    //OpenWeather not connecting
+    .catch(function(error) {
+      alert("Unable to connect to OpenWeather");
     });
 };
 
-    // search function
-    let searchSubmitHandler = function (event) {
-    // prevent refresh default
-    event.preventDefault();
+//searcg fuction
+let searchSubmitHandler = function(event) {
+  // stop page from refreshing
+  event.preventDefault();
 
-    // get value from input
-    let cityName = $("#cityname").val().trim();
-    if (cityName) {
-        getCityWeather(cityName);
+  // get value from input
+  let cityName = $("#cityname").val().trim();
 
-        // clear the search
-        $("#cityname").val("");
-    } else {
-        // if nothing was entered alert the user
-        alert("Please enter a city name");
-    }
+  // check if the search field has a value
+  if (cityName) {
+    // pass the value to getCityWeather function
+    getCityWeather(cityName);
+
+    // clear the search input
+    $("#cityname").val("");
+  } else {
+    // if nothing was entered alert the user
+    alert("Please enter a city name");
+  }
 };
 
-//function to append forecast
+// function to append forecast
 let displayWeather = function(weatherData) {
-    //weather info requested
-    $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
-    $("#city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
-    $("#city-humid").text("Humidity: " + weatherData.main.humidity + "%");
-    $("#city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
+  $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+  $("#main-city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+  $("#main-city-humid").text("Humidity: " + weatherData.main.humidity + "%");
+  $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
 
-    //5-day weather
-    fetch(
-        "https://api.openweathermap.org/data/2.5/forecast?q=" + weatherData.name + "&appid=c107fd0ee983a34881f3573465d57ca5&units=imperial")
-        .then(function (response) {
-        response.json().then(function (data) {
-        $("#five-day").empty();
+  //5-day weather
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + weatherData.name + "&appid=c107fd0ee983a34881f3573465d57ca5&units=imperial")
+    .then(function (response) {
+    response.json().then(function (data) {
+      // clear any previous entries in the five-day forecast
+      $("#five-day").empty();
 
-            for (i = 7; i <= data.list.length; i += 8) {
-                let fiveDayCard =`
-                    <div class="col-md-2 m-2 py-3 card text-white bg-primary">
-                        <div class="card-body p-1">
-                            <h5 class="card-title">` + dayjs(data.list[i].dt * 1000).format("MM/DD/YYYY") + `</h5>
-                            <img src="https://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `.png" alt="rain">
-                                <p class="card-text">Temp: ` + data.list[i].main.temp +`</p>
-                                <p class="card-text">Humidity: ` + data.list[i].main.humidity + `</p>
-                                <p class="card-text">Wind: ` +data.list[i].main.wind + `</p>
-                        </div>
-                    </div>`;
+      // get every 8th value (24hours) in the returned array from the api call
+      for (i = 7; i <= data.list.length; i += 8) {
+        // insert data into my day forecast card template
+        let fiveDayCard =`
+          <div class="col-md-2 m-2 py-3 card text-white bg-primary">
+            <div class="card-body p-1">
+              <h5 class="card-title">` + dayjs(data.list[i].dt * 1000).format("MM/DD/YYYY") + `</h5>
+              <img src="https://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `.png" alt="rain">
+                <p class="card-text">Temp: ` + data.list[i].main.temp + `</p>
+                <p class="card-text">Humidity: ` + data.list[i].main.humidity + `</p>
+            </div>
+          </div>`;
 
-                $("#five-day").append(fiveDayCard);
-            }
-        });
+        // append the day to the five-day forecast
+        $("#five-day").append(fiveDayCard);
+      }
     });
+  });
 
+  // save the last city searched
   lastCitySearched = weatherData.name;
+
+  // save to the search history using the api's name value for consistancy
+  // this also keeps searches that did not return a result from populating the array
   saveSearchHistory(weatherData.name);
 };
 
+// function to save the city search history to local storage
+let saveSearchHistory = function (city) {
+  if (!searchHistory.includes(city)) {
+    searchHistory.push(city);
+    $("#search-history").append(
+      "<a href='#' class='list-group-item list-group-item-action' id='" +
+        city +
+        "'>" +
+        city +
+        "</a>"
+    );
+  }
+
+  // save the searchHistory array to local storage
+  localStorage.setItem("weatherSearchHistory", JSON.stringify(searchHistory));
+
+  // save the lastCitySearched to local storage
+  localStorage.setItem("lastCitySearched", JSON.stringify(lastCitySearched));
+
+  // display the searchHistory array
+  loadSearchHistory();
+};
+
+// function to load saved city search history from local storage
+let loadSearchHistory = function () {
+  searchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+  lastCitySearched = JSON.parse(localStorage.getItem("lastCitySearched"));
+
+  // if nothing in localStorage, create an empty searchHistory array and an empty lastCitySearched string
+  if (!searchHistory) {
+    searchHistory = [];
+  }
+
+  if (!lastCitySearched) {
+    lastCitySearched = "";
+  }
+
+  // clear any previous values from th search-history ul
+  $("#search-history").empty();
+
+  // for loop that will run through all the citys found in the array
+  for (i = 0; i < searchHistory.length; i++) {
+    // add the city as a link, set it's id, and append it to the search-history ul
+    $("#search-history").append(
+      "<a href='#' class='list-group-item list-group-item-action' id='" +
+        searchHistory[i] +
+        "'>" +
+        searchHistory[i] +
+        "</a>"
+    );
+  }
+};
+
+// load search history from local storage
+loadSearchHistory();
+
+// start page with the last city searched if there is one
+if (lastCitySearched != "") {
+  getCityWeather(lastCitySearched);
+}
+
+// event handlers
+$("#search-form").submit(searchSubmitHandler);
+$("#search-history").on("click", function (event) {
+  // get the links id value
+  let prevCity = $(event.target).closest("a").attr("id");
+  // pass it's id value to the getCityWeather function
+  getCityWeather(prevCity);
+});
